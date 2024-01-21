d190d847b4c169fd3af91244b654ae5d33979d96
DHN Chandan <php.chandan@gmail.com>
2024-01-21 07:58:00 +0600

    :sparkles: feat: Make x and y axes selection dynamic

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import os
+import re
+import shlex
+import sys
+import typing
 from io import StringIO, BytesIO
 
 import matplotlib.pyplot as plt
+import numpy as np
+import numpy.typing as npt
 import streamlit as st
 from streamlit_option_menu import option_menu
 
@@ -23,49 +29,135 @@ selected = option_menu(
     menu_icon="cast", default_index=0, orientation="horizontal"
 )
 
+
+class Labels(typing.TypedDict):
+    xaxis: str
+    yaxis: str
+    series: list[str]
+
+
+class Metadata(typing.TypedDict):
+    title: str
+    labels: Labels
+
+
+def parse_xvg(data: str, columns: list[int] | None = None) -> tuple[Metadata, npt.NDArray]:
+    _ignored = {'legend', 'view'}
+    _re_series = re.compile('s[0-9]+$')
+    _re_axis = re.compile('[xy]axis$')
+
+    metadata: Metadata = {}
+    num_data = []
+
+    metadata['labels'] = {}
+    metadata['labels']['series'] = []
+
+    for line in data.split("\n"):
+        line = line.strip()
+
+        if line.startswith("@"):
+            tokens = shlex.split(line[1:])
+
+            if tokens[0] in _ignored:
+                continue
+            elif tokens[0] == "TYPE":
+                if tokens[1] != 'xy':
+                    print("Unsupported")
+            elif _re_series.match(tokens[0]):
+                metadata['labels']['series'].append(tokens[-1])
+            elif _re_axis.match(tokens[0]):
+                if "label" in tokens:
+                    if tokens[0] == "xaxis":
+                        metadata['labels']["xaxis"] = tokens[-1]
+                    else:
+                        metadata['labels']["yaxis"] = tokens[-1]
+            elif len(tokens) == 2:
+                if tokens[0] == "title":
+                    metadata["title"] = tokens[1]
+            else:
+                print('Unsupported entry: {0} - ignoring'.format(tokens[0]), file=sys.stderr)
+
+        elif len(line) and line[0].isdigit():
+            num_data.append(list(map(float, line.split())))
+
+    data: npt.NDArray = np.array(num_data)
+
+    if len(metadata['labels']['series']) < 1:
+        for index, series in enumerate(list(range(data.shape[1] - 1))):
+            metadata['labels']['series'].append(f"Series {index + 1}")
+
+    if columns:
+        sel_columns = [0] + list(map(lambda x: int(x + 1), columns))
+        data = data[..., sel_columns]
+
+        metadata['labels']['series'] = [metadata['labels']['series'][col - 1] for col in sel_columns[1:]]
+
+    return metadata, data
+
+
 if selected == "Single File Analysis":
     img = BytesIO()
+    file_name = ""
+    title = ""
+    xaxis = ""
+    series = []
+    xvg_file_name = ""
 
     wrapper_columns = st.columns([3, 2])
 
     with wrapper_columns[0]:
-        with st.form("xvg_form"):
-            files = st.columns([3, 2])
-            xvg = files[0].file_uploader(label="Upload XVG File", accept_multiple_files=False, type=["xvg"])
+        # with st.form("xvg_form"):
+        files = st.columns([3, 2])
+        xvg = files[0].file_uploader(label="Upload XVG File", accept_multiple_files=False, type=["xvg"])
 
-            file_name_columns = st.columns([1])
-            file_name = file_name_columns[0].text_input(label="File Name")
+        if xvg is not None:
+            stringio = StringIO(xvg.getvalue().decode("utf-8"))
 
-            label_columns = st.columns([1, 1])
-            x_label = label_columns[0].text_input(label="X Label")
-            y_label = label_columns[1].text_input(label="Y Label")
+            string_data = stringio.read()
 
-            index_columns = st.columns([1, 1])
-            x_index = index_columns[0].number_input(label="X Index", min_value=0, max_value=10, step=1)
-            y_index = index_columns[1].number_input(label="Y Index", min_value=1, max_value=10, step=1)
+            file_lines = string_data.split("\n")
 
-            size_columns = st.columns([1])
-            label_size = size_columns[0].slider(label="Label Size", min_value=12, max_value=24, step=1, value=16)
+            metadata, data = parse_xvg(string_data)
 
-            st.form_submit_button('Plot XVG')
+            # print(data.shape)
+            # print(metadata["title"])
+            # print(metadata["labels"]["series"])
 
+            title = metadata["title"]
+            series = metadata["labels"]["series"]
+            xaxis = metadata["labels"]["xaxis"]
+            xvg_file_name = os.path.splitext(xvg.name)[0]
+
+        file_name_columns = st.columns([1])
+        file_name = file_name_columns[0].text_input(label="File Name", value=xvg_file_name)
+
+        axis_columns = st.columns([1, 1])
+        x_index_ = axis_columns[0].radio("Select X Axis", [xaxis] + series, index=0)
+        # y_index_ = axis_columns[1].multiselect(
+        #     'Select Y Axes',
+        #     ["Time"] + series,
+        # )
+        y_index_ = axis_columns[1].radio("Select Y Axis", [xaxis] + series, index=0)
+
+        label_columns = st.columns([1, 1])
+        x_label = label_columns[0].text_input(label="X Label", value=x_index_)
+        y_label = label_columns[1].text_input(label="Y Label", value=y_index_)
+
+        x_index = ([xaxis] + series).index(x_index_)
+        y_index = ([xaxis] + series).index(y_index_)
+
+        size_columns = st.columns([1])
+        label_size = size_columns[0].slider(label="Label Size", min_value=12, max_value=24, step=1, value=16)
+
+        if st.button("Plot XVG"):
             if file_name and x_label and y_label and label_size:
-                if xvg is not None:
-                    stringio = StringIO(xvg.getvalue().decode("utf-8"))
+                x = data[..., x_index]
+                y = data[..., y_index]
 
-                    string_data = stringio.read()
-
-                    file_lines = string_data.split("\n")
-
-                    x = [float(line.split()[int(x_index)]) for line in file_lines if
-                         line and not (line.startswith('#') or line.startswith('@'))]
-                    y = [float(line.split()[int(y_index)]) / 1000 for line in file_lines if
-                         line and not (line.startswith('#') or line.startswith('@'))]
-
-                    plt.plot(x, y)
-                    plt.xlabel(fr"{x_label}", fontsize=label_size)
-                    plt.ylabel(fr"{y_label}", fontsize=label_size)
-                    plt.savefig(img, format='png', dpi=600)
+                plt.plot(x, y)
+                plt.xlabel(fr"{x_label}", fontsize=label_size)
+                plt.ylabel(fr"{y_label}", fontsize=label_size)
+                plt.savefig(img, format='png', dpi=600)
 
     with wrapper_columns[1]:
         with st.container(border=True):
