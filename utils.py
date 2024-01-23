643045f9f888273a175e871368f2ebea43e8cdaa
DHN Chandan <php.chandan@gmail.com>
2024-01-24 04:57:31 +0600

    :sparkles: feat: Convert latex in label and legend

--- utils.py
+++ utils.py
@@ -26,8 +26,11 @@ def parse_xvg(data: str, columns: list[int] | None = None) -> tuple[Metadata, np
     metadata: Metadata = {}
     num_data = []
 
-    metadata['labels'] = {}
-    metadata['labels']['series'] = []
+    metadata['labels'] = {
+        "xaxis": "",
+        "yaxis": "",
+        "series": [],
+    }
 
     for line in data.split("\n"):
         line = line.strip()
@@ -41,13 +44,13 @@ def parse_xvg(data: str, columns: list[int] | None = None) -> tuple[Metadata, np
                 if tokens[1] != 'xy':
                     print("Unsupported")
             elif _re_series.match(tokens[0]):
-                metadata['labels']['series'].append(tokens[-1])
+                metadata['labels']['series'].append(clean_label(tokens[-1]))
             elif _re_axis.match(tokens[0]):
                 if "label" in tokens:
                     if tokens[0] == "xaxis":
-                        metadata['labels']["xaxis"] = tokens[-1]
+                        metadata['labels']["xaxis"] = clean_label(tokens[-1])
                     else:
-                        metadata['labels']["yaxis"] = tokens[-1]
+                        metadata['labels']["yaxis"] = clean_label(tokens[-1])
             elif len(tokens) == 2:
                 if tokens[0] == "title":
                     metadata["title"] = tokens[1]
@@ -76,3 +79,106 @@ legend_locations = [
     "best", "upper right", "upper left", "lower left", "lower right", "right", "center left", "center right",
     "lower center", "upper center", "center"
 ]
+
+
+def clean_label(string):
+    """Parses Grace label syntax into LaTeX"""
+
+    substring = string
+    chunks = []
+
+    while substring:
+        for key in _all_codes:
+            if substring.startswith(key):
+                chunks.append(key)
+                substring = substring[len(key):]
+                break
+
+        else:
+            chunks.append(substring[0])
+            substring = substring[1:]
+
+    chunks = [c for c in chunks if c not in _ignorable_codes]
+
+    string = ''
+
+    # sub/superscripts
+    for chunk in chunks:
+        if chunk == "\\s":
+            string += r'}_{\text{'
+        elif chunk == "\\S":
+            string += r'}^{\text{'
+        elif chunk == "\\N" or chunk == "\\v{}\\z{}":
+            string += r'}}\text{'
+        else:
+            string += chunk
+
+    string = r'$\text{' + string + '}$'
+
+    return string
+
+
+_font_codes = {
+    "\\f{x}": 'switch to font named "x"',
+    "\\f{n}": 'switch to font number n',
+    "\\f{}": 'return to original font',
+    "\\x": 'switch to Symbol font (same as \\f{Symbol})',
+}
+
+_color_codes = {
+    "\\R{x}": 'switch to color named "x"',
+    "\\R{n}": 'switch to color number n',
+    "\\R{}": 'return to original color',
+}
+
+_style_codes = {
+    "\\u": 'begin underline',
+    "\\U": 'stop underline',
+    "\\o": 'begin overline',
+    "\\O": 'stop overline',
+    "\\q": 'make font oblique (same as \\l{0.25})',
+    "\\Q": 'undo oblique (same as \\l{-0.25})',
+}
+
+_script_codes = {
+    "\\s": 'begin subscripting (same as \\v{-0.4}\\z{0.71})',
+    "\\S": 'begin superscripting (same as \\v{0.6}\\z{0.71})',
+    "\\N": 'return to normal style (same as \\v{}\\t{})',
+    "\\v{}\\z{}": 'return to normal style',
+}
+
+_other_codes = {
+    "\\#{x}": 'treat "x" (must be of even length) as list of hexadecimal char codes',
+    "\\t{xx xy yx yy}": 'apply transformation matrix',
+    "\\t{}": 'reset transformation matrix',
+    "\\z{x}": 'zoom x times',
+    "\\z{}": 'return to original zoom',
+    "\\r{x}": 'rotate by x degrees',
+    "\\l{x}": 'slant by factor x',
+    "\\v{x}": 'shift vertically by x',
+    "\\v{}": 'return to unshifted baseline',
+    "\\V{x}": 'shift baseline by x',
+    "\\V{}": 'reset baseline',
+    "\\h{x}": 'horizontal shift by x',
+    "\\n": 'new line',
+    "\\Fk": 'enable kerning',
+    "\\FK": 'disable kerning',
+    "\\Fl": 'enable ligatures',
+    "\\FL": 'disable ligatures',
+    "\\m{n}": 'mark current position as n',
+    "\\M{n}": 'return to saved position n',
+    "\\dl": 'LtoR substring direction',
+    "\\dr": 'RtoL substring direction',
+    "\\dL": 'LtoR text advancing',
+    "\\dR": 'RtoL text advancing',
+    "\\+": 'increase size (same as \\z{1.19} ; 1.19 = sqrt(sqrt(2)))',
+    "\\-": 'decrease size (same as \\z{0.84} ; 0.84 = 1/sqrt(sqrt(2)))',
+    "\\T{xx xy yx yy}": 'same as \\t{}\\t{xx xy yx yy}',
+    "\\Z{x}": 'absolute zoom x times (same as \\z{}\\z{x})',
+    "\\\\": 'print \\',
+    "\\c": 'begin using upper 128 characters of set (deprecated)',
+    "\\C": 'stop using upper 128 characters of set (deprecated)',
+}
+
+_ignorable_codes = _font_codes | _color_codes | _style_codes | _other_codes
+_all_codes = _script_codes | _font_codes | _style_codes | _color_codes | _other_codes
